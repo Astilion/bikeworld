633efd9449bdce9deedebc0c54ed8fe21890f7db
Mateusz <mkaminski.contact@gmail.com>
2023-09-24 10:35:08 +0200

    added icons + styled them

--- src/components/userPanel/UserPanel.jsx
+++ src/components/userPanel/UserPanel.jsx
@@ -3,10 +3,10 @@ import styles from "./UserPanel.module.scss";
 const UserPanel = props => {
 	return (
 		<div className={styles.panel}>
-			<button>menu</button>
-			<button>Search</button>
-			<button>Account</button>
-			<button>Cart</button>
+			<button><i class="ti ti-menu-2"></i></button>
+			<button><i class="ti ti-search"></i></button>
+			<button><i class="ti ti-user-hexagon"></i></button>
+			<button><i class="ti ti-shopping-cart"></i></button>
 		</div>
 	);
 };
