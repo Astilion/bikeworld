451cc891f34dd9b589bb12378a6aa23a48174f3d
Mateusz <mkaminski.contact@gmail.com>
2023-09-25 10:45:17 +0200

    responsive adding UserPanel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import './App.scss'
+import { useState, useEffect } from 'react'
 import Header from './components/layout/Header';
 import UserPanel from './components/userPanel/userPanel';
 const App =() => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const isShown = screenWidth >= 768
   return (
     <>
  
     <Header/>
-
-    <UserPanel/>
+    {isShown ? null : <UserPanel/>}
     </>
   );
 }
