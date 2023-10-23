13c38b418312e7f3c7faeb6032d7de1453873856
Mateusz <mkaminski.contact@gmail.com>
2023-10-23 10:46:26 +0200

    added draggable version of mobile slider

--- src/components/bestsellers/Slider.tsx
+++ src/components/bestsellers/Slider.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import Card from "./Card";
 import styles from "./Slider.module.scss";
 
@@ -8,8 +8,15 @@ import bike3 from "../../assets/Rower-gorski.jpg";
 import bike4 from "../../assets/WYGODNE-SIODELKO-ROWEROWE-SPORTOWE-PIANKA-MIEKKIE.jpg";
 import bike5 from "../../assets/KLAMKOMANETKA-SHIMANO-Acera-STEF-500-3x7.jpg";
 
+type SlideData = {
+	id: number;
+	name: string;
+	price: string;
+	description: string;
+	url: string;
+};
 
-const slideData = [
+const slideData: SlideData[] = [
 	{
 		id: 1,
 		name: "Mountain bike 1",
@@ -93,28 +100,66 @@ const Slider = () => {
 
 	const cardWidth = 256 + 10;
 	const cardsToScroll = 1;
-	
+
+	let isDragging = false;
+	let startX: number = 0;
+
+	const handleStart = (e: TouchEvent) => {
+		isDragging = true;
+		startX = e.touches[0].clientX;
+	};
+
+	const handleMove = (e: TouchEvent) => {
+		if (!isDragging) return;
+		const x = e.touches[0].clientX;
+		const walk = (x - startX) * 20;
+		if (carouselRef.current) {
+			carouselRef.current.scrollLeft -= walk;
+		}
+		startX = x;
+	};
+
+	const handleEnd = () => {
+		isDragging = false;
+	};
+
+	useEffect(() => {
+		if (carouselRef.current) {
+			carouselRef.current.ontouchstart = handleStart;
+			carouselRef.current.ontouchmove = handleMove;
+			carouselRef.current.ontouchend = handleEnd;
+		}
+
+		return () => {
+			if (carouselRef.current) {
+				carouselRef.current.ontouchstart = null;
+				carouselRef.current.ontouchmove = null;
+				carouselRef.current.ontouchend = null;
+			}
+		};
+	}, []);
+
 	const prev = () => {
 		const box = carouselRef.current;
 		if (box) {
-		  if (box.scrollLeft === 0) {
-			box.scrollLeft = box.scrollWidth;
-		  } else {
-			box.scrollLeft = box.scrollLeft - cardWidth * cardsToScroll;
-		  }
+			if (box.scrollLeft === 0) {
+				box.scrollLeft = box.scrollWidth;
+			} else {
+				box.scrollLeft = box.scrollLeft - cardWidth * cardsToScroll;
+			}
 		}
-	  };
-	
-	  const next = () => {
+	};
+
+	const next = () => {
 		const box = carouselRef.current;
 		if (box) {
-		  if (box.scrollLeft >= box.scrollWidth - box.clientWidth) {
-			box.scrollLeft = 0;
-		  } else {
-			box.scrollLeft = box.scrollLeft + cardWidth * cardsToScroll;
-		  }
+			if (box.scrollLeft >= box.scrollWidth - box.clientWidth) {
+				box.scrollLeft = 0;
+			} else {
+				box.scrollLeft = box.scrollLeft + cardWidth * cardsToScroll;
+			}
 		}
-	  };
+	};
 
 	const carousel = (
 		<div className={styles.carousel} ref={carouselRef}>
@@ -133,15 +178,14 @@ const Slider = () => {
 	return (
 		<div className={styles.slider}>
 			{carousel}
-			<div className={styles['buttons-container']}>
-			<button className={styles['prev-button']} onClick={prev}>
-			<i className="ti ti-arrow-badge-left"></i>
-			</button>
-			<button className={styles['next-button']} onClick={next}>
-			<i className="ti ti-arrow-badge-right"></i>
-			</button>
+			<div className={styles["buttons-container"]}>
+				<button className={styles["prev-button"]} onClick={prev}>
+					<i className='ti ti-arrow-badge-left'></i>
+				</button>
+				<button className={styles["next-button"]} onClick={next}>
+					<i className='ti ti-arrow-badge-right'></i>
+				</button>
 			</div>
-
 		</div>
 	);
 };
